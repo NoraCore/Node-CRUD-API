9395fb4c54542f0ddd539cfd585aede4902f30a9
Julia <juliakrivonos22@gmail.com>
2025-11-10 00:14:22 +0300

    feat: add repository layer

---
+++ src/repositories/repository.ts
@@ -0,0 +1,43 @@
+import { UUID, randomUUID } from 'node:crypto';
+
+export interface Repository<T extends { id: UUID }> {
+  getById(id: string): Promise<T>;
+  getAll(): Promise<T[]>;
+  create(item: T): Promise<T>;
+  update(item: T): Promise<T | null>;
+  delete(id: UUID): Promise<T | null>;
+}
+
+export class InMemoryRepository<T extends { id: UUID }> implements Repository<T> {
+  private items: Map<string, T> = new Map();
+
+  async create(item: T): Promise<T> {
+    const id = randomUUID()
+    this.items.set(id, item)
+    return item;
+  }
+
+  async delete(id: UUID): Promise<T | null> {
+    const item = this.getById(id);
+    if (!item){
+      return null
+    }
+    return this.items.delete(id) ? item : null;
+  }
+
+  async getAll(): Promise<T[]> {
+    return Array.from(this.items.values())
+  }
+
+  async update(item: T): Promise<T | null> {
+    if (!this.items.has(item.id)){
+      return null
+    }
+    this.items.set(item.id, item);
+    return item;
+  }
+
+  async getById(id: string): Promise<T | null> {
+    return this.items.get(id) || null;
+  }
+}
---
+++ src/repositories/userRepository.ts
@@ -0,0 +1,4 @@
+import { User } from '../models/userModel';
+import { InMemoryRepository } from './repository';
+
+export class UserRepository extends InMemoryRepository<User> {}
